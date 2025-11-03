47c7b441c651ef3c88e25ef152cc498c439de186
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 00:39:57 +0530

    Defined types.d.ts connections

---
+++ lib/Db.ts
@@ -0,0 +1,36 @@
+import { channel } from "diagnostics_channel";
+import mongoose from "mongoose";
+
+const MONGO_URl=process.env.MONGO_URL;
+if(!MONGO_URl) throw new Error("MONGO_URL is not defined in environment variables");
+
+let cashed=global.mongoose
+if(!cashed){
+    cashed={cnn:null,promise:null};
+    global.mongoose=cashed;
+}
+export async function connectDb(){
+    const opt={
+        bufferCommands:true,
+        useNewUrlParser:true,
+        maxpullSize:10,
+        wtimeoutMS:2500,
+        useUnifiedTopology:true,
+    };
+     if(cashed.cnn){ return chashed.cnn;
+     };
+     if(!cashed.promise){
+        mongoose.connect(
+            MONGO_URl,
+            opt
+        ).then(()=>mongoose.connections[0]);
+     }
+     try {
+        await cashed.promise;
+        return cashed.cnn;
+     } catch (error) {
+        cashed.promise=null;
+        throw error;    
+     }
+     return cashed.cnn;
+}
