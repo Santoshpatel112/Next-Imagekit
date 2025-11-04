b9209acf4d8cf86465ab6d315c79830e9d37f507
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 11:58:58 +0530

    api/auth/register

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,26 @@
+import { connectDb } from "@/lib/Db";
+import User from "@/models/User";
+import { error } from "console";
+import { NextRequest ,NextResponse } from "next/server";
+
+export async function POST(req :NextRequest){
+    try {
+        const {email ,password} =await req.json();
+        if(!email ||!password){
+            return NextResponse.json({error:"Email and password are Required"},{status:400});
+        }
+        await connectDb();
+        const exitinguser=await User.findOne({email});
+        if(exitinguser){
+            return NextResponse.json({error:"User already exists"},{status:400});
+        }
+        const newUser= await User.create({
+            email,
+            password,
+        });
+        return NextResponse.json({message:"User registered successfully"
+        },{status:201});
+    } catch (error) {
+        return NextResponse.json({error:"Internal Server Error"},{status:500});
+    }
+}
