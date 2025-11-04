e8aca906dd27175cee899215489463643bed8425
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-05 00:23:26 +0530

    register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 import React from "react"
-import { useState } from "react";
+import {useState } from "react";
+import {useRouter} from 'next/navigation';
 export default function RegisterPage(){
     const [email ,setEmail]=useState('');
     const [password ,setPassword]=useState('');
     const [confirmPassword ,setConfirmPassword]=useState('');
+    const router=useRouter();
+
+
+    
     return(
         <div>
             <h1>Register Page</h1>
