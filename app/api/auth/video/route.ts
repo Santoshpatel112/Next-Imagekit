9c036f994c00b2cf4340cc6201b565b488840fd5
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 19:03:59 +0530

    Video post  method

--- app/api/auth/video/route.ts
+++ app/api/auth/video/route.ts
@@ -1,6 +1,8 @@
 import { connectDb } from "@/lib/Db";
-import Video from "@/Models/Video";
-import { NextResponse } from "next/server";
+import Video, { IVideo } from "@/Models/Video";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth/next";
+import { NextResponse ,NextRequest } from "next/server";
 
 export async function GET(){
     try {
@@ -15,5 +17,36 @@ export async function GET(){
     } catch (error) {
         console.error("Error fetching videos:", error);
         return NextResponse.json({message:"Feiled to fetch Video",error}, {status:500});
+    }
+}
+
+
+export async function POST(request:Request){
+    try {
+        const session=await getServerSession(authOptions);
+        if(!session){
+            return NextResponse.json({message:"Unauthorized"}, {status:401});
+        }
+        await connectDb();
+        const body:IVideo=await request.json();
+        if(!body.title ||!body.Description ||!body.videoUrl ||!body.thumbnailUrl){
+            return NextResponse.json({message:"All fields are required"}, {status:400});
+        }
+        const videodata={
+            title:body.title,
+            Description:body.Description,
+            videoUrl:body.videoUrl,
+            thumbnailUrl:body.thumbnailUrl,
+            controls:body.controls ||true,
+            trinsformation:{
+                height:body.trinsformation?.height ||1920,
+                width:body.trinsformation?.width ||1080,
+                quility:body.trinsformation?.quility ||75,
+            }
+        };
+        const newvideo=await Video.create(videodata);
+        return NextResponse.json({message:"Video uploaded successfully",video:newvideo}, {status:201});
+    } catch (error) {
+        return NextResponse.json({message:"Failed to upload video",error}, {status:500});
     }
 }
