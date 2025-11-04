b88afda1e71e4e20e5502348a841dcb880cba5db
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 13:07:41 +0530

    auth.ts credential provider custom

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,43 @@
-export const authOptions = {};
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/Models/User";
+import { connectDb } from "./Db";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+export const authOptions :NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials:{
+                email:{label:"Email",type:"text"},
+                password:{label:"Password",type:"password"}
+            },
+            async authorize(credentials){
+                // custom login logic
+                if(!credentials?.email ||! credentials?.password){
+                    throw new Error("Email and Password are required");
+                }
+
+                try {
+                    await connectDb();
+                    const user= await User.findOne({email:credentials.email});
+                    if(!user){
+                        throw new Error("No user found with the given email");
+                    }
+                    const ispasswordValid=await bcrypt.compare(credentials.password,user.password);
+                    if(!ispasswordValid){
+                        throw new Error("Invalid password");
+                    }
+                    return {
+                        id:user._id.toString(), // Ensure id is a string
+                        email:user.email,
+                    };
+                } catch (error) {
+                    console.log("Error in user login",error);
+                    return NextResponse.json({error:"Internal Server Error"},{status:500});
+                }
+            }
+        })
+
+]
+};
