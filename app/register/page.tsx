e37f44e083f14b0305cb157d9323ee7b16a14552
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 19:28:42 +0530

    usestate

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,8 @@ import React from "react"
 import { useState } from "react";
 export default function RegisterPage(){
     const [email ,setEmail]=useState('');
+    const [password ,setPassword]=useState('');
+    const [confirmPassword ,setConfirmPassword]=useState('');
     return(
         <div>
             <h1>Register Page</h1>
