a8cf9596912a1c305aff5bd07f9476a67c3d54f6
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 13:33:02 +0530

    defined callback session and token

--- lib/auth.ts
+++ lib/auth.ts
@@ -38,6 +38,20 @@ export const authOptions :NextAuthOptions = {
                 }
             }
         })
+],
+callbacks:{
+    async jwt({token,user}){
+        if(user){
+            token.id=user.id;
+        }
+        return token;
+    },
+     async session({session,token}){
+        if(session.user){
+            session.user.id=token.id
+        }
+        return session;
+    }
+}
 
-]
 };
