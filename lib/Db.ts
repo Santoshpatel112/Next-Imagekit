0c89ad5d2386537866fd4db5268423ea9123a681
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 13:55:36 +0530

    Fix auth and database connection errors

--- lib/Db.ts
+++ lib/Db.ts
@@ -1,35 +1,33 @@
 import mongoose from "mongoose";
 
-const MONGO_URl=process.env.MONGO_URL;
-if(!MONGO_URl) throw new Error("MONGO_URL is not defined in environment variables");
+const MONGO_URl = process.env.MONGO_URL as string;
+if (!MONGO_URl)
+  throw new Error("MONGO_URL is not defined in environment variables");
 
-let cashed=global.mongoose
-if(!cashed){
-    cashed={cnn:null,promise:null};
-    global.mongoose= cashed={cnn:null,promise:null};
+let cashed = global.mongoose;
+if (!cashed) {
+  cashed = { cnn: null, promise: null };
+  global.mongoose = cashed = { cnn: null, promise: null };
 }
-export async function connectDb(){
-    const opt={
-        bufferCommands:true,
-        useNewUrlParser:true,
-        maxpullSize:10,
-        wtimeoutMS:2500,
-        useUnifiedTopology:true,
-    };
-     if(cashed.cnn){ return chashed.cnn;
-     };
-     if(!cashed.promise){
-        mongoose.connect(
-            MONGO_URl,
-            opt
-        ).then(()=>mongoose.connections[0]);
-     }
-     try {
-        await cashed.promise;
-        return cashed.cnn;
-     } catch (error) {
-        cashed.promise=null;
-        throw error;    
-     }
-     return cashed.cnn;
+export async function connectDb() {
+  const opt = {
+    bufferCommands: true,
+    maxPoolSize: 10,
+    wtimeoutMS: 2500,
+  };
+  if (cashed.cnn) {
+    return cashed.cnn;
+  }
+  if (!cashed.promise) {
+    cashed.promise = mongoose
+      .connect(MONGO_URl, opt)
+      .then((mongoose) => mongoose.connection);
+  }
+  try {
+    cashed.cnn = await cashed.promise;
+    return cashed.cnn;
+  } catch (error) {
+    cashed.promise = null;
+    throw error;
+  }
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -4,63 +4,65 @@ import User from "@/Models/User";
 import { connectDb } from "./Db";
 import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
-export const authOptions :NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials:{
-                email:{label:"Email",type:"text"},
-                password:{label:"Password",type:"password"}
-            },
-            async authorize(credentials){
-                // custom login logic
-                if(!credentials?.email ||! credentials?.password){
-                    throw new Error("Email and Password are required");
-                }
-
-                try {
-                    await connectDb();
-                    const user= await User.findOne({email:credentials.email});
-                    if(!user){
-                        throw new Error("No user found with the given email");
-                    }
-                    const ispasswordValid=await bcrypt.compare(credentials.password,user.password);
-                    if(!ispasswordValid){
-                        throw new Error("Invalid password");
-                    }
-                    return {
-                        id:user._id.toString(), // Ensure id is a string
-                        email:user.email,
-                    };
-                } catch (error) {
-                    console.log("Error in user login",error);
-                    return NextResponse.json({error:"Internal Server Error"},{status:500});
-                }
-            }
-        })
-],
-callbacks:{
-    async jwt({token,user}){
-        if(user){
-            token.id=user.id;
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // custom login logic
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and Password are required");
         }
-        return token;
+
+        try {
+          await connectDb();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("No user found with the given email");
+          }
+          const ispasswordValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!ispasswordValid) {
+            throw new Error("Invalid password");
+          }
+          return {
+            id: user._id.toString(), // Ensure id is a string
+            email: user.email,
+          };
+        } catch (error) {
+          console.log("Error in user login", error);
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
     },
-     async session({session,token}){
-        if(session.user){
-            session.user.id=token.id as string 
-        }
-        return session;
-    }
-},
-pages:{
-    signIn:"/login",
-    error:"/login",
-},
-session:{
-    strategy:"jwt",
-    maxAge:30*24*60*60, //30 days
-},
-secret:process.env.NEXTAUTH_SECRET,
-
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, //30 days
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
