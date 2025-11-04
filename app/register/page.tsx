3c07979f39c84db4a8873c20fa2f37b2e9fbe1c1
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-04 19:27:30 +0530

    registerpage components setup use client directive

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import React from "react"
-
+import { useState } from "react";
 export default function RegisterPage(){
+    const [email ,setEmail]=useState('');
     return(
         <div>
             <h1>Register Page</h1>
