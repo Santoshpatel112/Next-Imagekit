677350948a2c766c49abf22a869d638f4d9aae31
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-05 16:52:33 +0530

    Secrate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "./Components/providers";
+import Providers from "./Components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
