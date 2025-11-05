e2ac21eb47bcd19c92e06347e106a3eecfac363b
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-11-05 17:03:24 +0530

    fix error

--- app/Components/providers.tsx
+++ app/Components/providers.tsx
@@ -1,14 +1,14 @@
+"use client";
 import React from "react";
-import {SessionProvider} from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 import { ImageKitProvider } from "@imagekit/next";
 
+const urlEndpoint = process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!;
 
-const urlEndpoint=process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!;
-
-export default function Providers({children}:{children:React.ReactNode}){
-    return <SessionProvider refetchInterval={5*60}>
-        <ImageKitProvider urlEndpoint={urlEndpoint}>
-            {children}
-        </ImageKitProvider>
-    </SessionProvider>;
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider refetchInterval={5 * 60}>
+      <ImageKitProvider urlEndpoint={urlEndpoint}>{children}</ImageKitProvider>
+    </SessionProvider>
+  );
 }
